2ff4bd5630470bfeedd7b6c68bfa8ce9f975e353
yeona813 <129318957+yeona813@users.noreply.github.com>
2023-07-28 20:19:34 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int a, int b)
-{
+int gcd(int a, int b){
     int c;
-    while (b != 0)
-    {
+    while (b != 0) {
         c = a % b;
         a = b;
         b = c;
@@ -14,16 +12,15 @@ int gcd(int a, int b)
     return a;
 } // 최대공약수 구하는 것(유클리드 호제법 이용)
 
-int lcm(int a, int b)
-{
+int lcm(int a, int b){
     return a * b / gcd(a, b);
 } // 최소공배수 구하는 것
 
-int main(void)
-{
-    int a, b;
-    cin >> a >> b;
-    if (a > b) cout << gcd(a, b) << " " << lcm(a, b) << endl;
-    else cout << gcd(b, a) << " " << lcm(b, a) << endl;
+int main(void){
+    ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+    int a, b; cin >> a >> b;
+    cout << gcd(a, b) << " " << lcm(a, b) << endl;
     return 0;
 }
