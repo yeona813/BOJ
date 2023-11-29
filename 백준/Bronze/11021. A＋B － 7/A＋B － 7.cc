091e70b824b8055cd75115113b8846c7f021faeb
yeona813 <129318957+yeona813@users.noreply.github.com>
2023-11-29 23:34:38 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	int N, num1, num2;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> num1 >> num2;
+		cout << "Case #" << i + 1 << ": " << num1 + num2 << endl;
+	}
+
+	return 0;
+}
+
