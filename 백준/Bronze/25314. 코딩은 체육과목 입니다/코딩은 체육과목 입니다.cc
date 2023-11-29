6afa321c753dea3b33203a1aafb03f493ca67891
yeona813 <129318957+yeona813@users.noreply.github.com>
2023-11-29 23:29:11 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+
+	int N;
+	cin >> N;
+	for (int i = 0; i < N / 4; i++) {
+		cout << "long ";
+	}
+	cout << "int" << endl;
+
+	return 0;
+}
