29c7f96f08e6c023ed3a4d0392d6b27f60b48f6d
yeona813 <129318957+yeona813@users.noreply.github.com>
2023-11-30 23:22:42 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,16 @@
+function solution(price) {
+    var answer = 0;
+    if(price >= 500000){
+        answer = Math.floor(price*0.8);
+    }
+    else if(price >= 300000){
+        answer = Math.floor(price*0.9);
+    }
+    else if(price >= 100000){
+        answer = Math.floor(price*0.95);
+    }
+    else {
+        answer = price;
+    }
+    return answer;
+}
