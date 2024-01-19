ea7c1c4a179e73f4bbb5838d9c5918bf75e70f3f
yeona813 <129318957+yeona813@users.noreply.github.com>
2024-01-19 14:21:44 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,13 @@
+function solution(price, money, count) {
+    var answer = 0;
+    for(let i = 1; i<=count; i++){
+        answer += price*i;
+    }
+    answer -= money;
+    
+    if(answer < 0) {
+        return 0;
+    }
+
+    return answer;
+}
