95e1673367afcfee377e91cd1bc2d34c433c01a0
yeona813 <129318957+yeona813@users.noreply.github.com>
2024-01-24 10:03:12 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 10.88 ms, Memory: 38.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    let answer = [];
+    let stack = [];
+    
+    for(let i = 0; i<s.length; i++){
+        if(!stack.includes(s[i])){
+            answer.push(-1);
+            stack.push(s[i]);
+        }
+        else{
+            answer.push(stack.length-stack.lastIndexOf(s[i]));
+            stack.push(s[i]);
+        }
+    }
+    return answer;
+}
