1e6b9830df343c6e6f9e2e3fb1a028e052b4bab2
yeona813 <129318957+yeona813@users.noreply.github.com>
2024-03-03 20:30:38 +0900

    [Silver V] Title: 식당 입구 대기 줄, Time: 684 ms, Memory: 26868 KB -BaekjoonHub

---
+++ 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const { resourceUsage } = require("process");
+
+// const input = fs.readFileSync("./input.txt", "utf8").trim().split("\n");
+
+const input = fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
+
+const n = Number(input[0]);
+
+const queue = [];
+let maxLine = 0;
+let minPeople = 0;
+
+for (let i = 1; i <= n; i++) {
+  let [cmd, value] = input[i].split(" ").map(Number);
+  if (cmd === 1) {
+    queue.push(value);
+    if (maxLine < queue.length) {
+      maxLine = queue.length;
+      minPeople = queue[queue.length - 1];
+    } else if (
+      maxLine === queue.length &&
+      queue[queue.length - 1] < minPeople
+    ) {
+      minPeople = queue[queue.length - 1];
+    }
+  } else if (cmd === 2) {
+    if (maxLine < queue.length) {
+      maxLine = queue.length;
+      minPeople = queue[queue.length - 1];
+    } else if (
+      maxLine === queue.length &&
+      queue[queue.length - 1] < minPeople
+    ) {
+      minPeople = queue[queue.length - 1];
+    }
+    queue.shift();
+  }
+}
+
+console.log(maxLine, minPeople);
+
+
