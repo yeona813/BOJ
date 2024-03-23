2c8c068006802ca309098c185334cd2daf36f077
yeona813 <129318957+yeona813@users.noreply.github.com>
2024-03-23 23:33:20 +0900

    [Silver V] Title: 돌 게임, Time: 200 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.js
+++ 백준/Silver/9655. 돌 게임/돌 게임.js
@@ -2,14 +2,8 @@ const fs = require("fs");
 const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let N = Number(fs.readFileSync(path).toString());
 
-const dp = (num) => {
-  if (num === 1) return "SK";
-  if (num === 2) return "CY";
-  const DP = [0, 1, 2];
-  for (let i = 3; i <= num; i++) {
-    DP[i] = Math.min(DP[i - 1] + 1, DP[i - 3] + 1);
-  }
-  return DP[num] % 2 === 0 ? "CY" : "SK";
-};
-
-console.log(dp(N));
+if (N % 2 === 0) {
+  console.log("CY");
+} else {
+  console.log("SK");
+}
